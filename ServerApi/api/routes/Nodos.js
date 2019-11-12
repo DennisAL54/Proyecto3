6be52705c968892d4a1b1c6ef78408246094bec2
Unknown <alejimenezc@hotmail.com>
2019-11-12 17:28:00 -0600

    Funciones basicas (esqueleto)

--- ServerApi/api/routes/Nodos.js
+++ ServerApi/api/routes/Nodos.js
@@ -13,4 +13,28 @@ router.post('/', (req, res, next) => {
 	});
 });
 
+router.get('/:NodeID', (req, res, next) => {
+	const id = req.params.NodeID;
+	if (id === 'nodo1'){
+		res.status(200).json({
+		    message: 'id encontrado',
+		    id: id
+	});
+	}else{
+	   res.status(200).json({
+	       message: 'pasaste un id'
+	   });
+	}
+});
+router.patch('/:NodeID', (req, res, next) => {
+	res.status(200).json({
+		message: 'Nodo Actualizado'
+	})
+})
+router.delete('/:NodeID', (req, res, next) => {
+	res.status(200).json({
+		message: 'Nodo Borrado'
+	})
+})
+
 module.exports = router;
