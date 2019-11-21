7a5cc5ef51791cbb22259c0efd6767987354cb9d
Unknown <alejimenezc@hotmail.com>
2019-11-20 19:16:16 -0600

    Requests necesarios (Pendiente la acción)

--- ServerApi/api/routes/Aristas.js
+++ ServerApi/api/routes/Aristas.js
@@ -1,4 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next)=> {
+    res.status(200).json()({
+        message:'Arista obtenida'
+    })
+})
+router.post('/', (req, res, next) => {
+	res.status(201).json ({
+	    message: 'Arista creada'
+	});
+});
+
+router.get('/:EdgeId', (req, res, next)=> {
+    res.status(200).json()({
+        message:'Detalles de la arista',
+        EdgeId: req.params.EdgeId
+    })
+})
+router.delete('/:EdgeId', (req, res, next)=> {
+    res.status(200).json()({
+        message:'Arista Borrada',
+        EdgeId: req.params.EdgeId
+    })
+})
+
+
+
+
+
 module.exports = router;
